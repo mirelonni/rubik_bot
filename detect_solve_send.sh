40c9c9a3a76cf3e1d120fe12df82060bd4ec58d2
Mircea Cojocaru <mirelonni@gmail.com>
2019-05-24 10:50:48 +0300

    fixed starting script

--- detect_solve_send.sh
+++ detect_solve_send.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# ./color_detect/color
-#
-# ./ckociemba/bin/kociemba $(cat state.txt) > solution.txt
+./color_detect/color
+
+./ckociemba/bin/kociemba $(cat state.txt) > solution.txt
 
 echo "$1 $2 $(cat solution.txt)#"
-# sudo stty 9600 -F /dev/ttyUSB0
-# sudo chmod o+rw /dev/ttyUSB0
-# sudo echo "1 700 $(cat solution.txt)#" > /dev/ttyUSB0
+sudo stty 9600 -F /dev/ttyUSB0
+sudo chmod o+rw /dev/ttyUSB0
+sudo echo "1 700 $(cat solution.txt)#" > /dev/ttyUSB0
